a527db248eb65cb518d21ed9952d621fac35f545
Xuanwen <xwghua@gmail.com>
2022-05-14 11:38:43 -0400

    getdir update

--- getDir.m
+++ getDir.m
@@ -5,14 +5,16 @@ catch
     dirname = uigetdir('./');
     warning(['Cannot find "',inputpath,'", redirected to "',dirname,'".']);
 end
-if length(dirname)==1
-    dirname = './';
-end
-if nargin>1
-%     disp([dirname,'\*.',varargin{1}])
-    components = dir([dirname,'/*.',varargin{1}]);
+if length(dirname)~=1
+    if nargin>1
+        %     disp([dirname,'\*.',varargin{1}])
+        components = dir([dirname,'/*.',varargin{1}]);
+    else
+        components = dir(dirname);
+        components = components(3:end);
+    end
 else
-    components = dir(dirname);
-    components = components(3:end);
+    components = {};
 end
+
 end
