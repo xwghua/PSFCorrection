8d11418a49ab91553b654f503187aaa96432c087
Xuanwen <xwghua@gmail.com>
2022-05-14 02:07:40 -0400

    support macos

--- getDir.m
+++ getDir.m
@@ -2,15 +2,15 @@ function [dirname,components] = getDir(inputpath,varargin)
 try
     dirname = uigetdir(inputpath);
 catch
-    dirname = uigetdir('.\');
+    dirname = uigetdir('./');
     warning(['Cannot find "',inputpath,'", redirected to "',dirname,'".']);
 end
 if length(dirname)==1
-    dirname = '.\';
+    dirname = './';
 end
 if nargin>1
 %     disp([dirname,'\*.',varargin{1}])
-    components = dir([dirname,'\*.',varargin{1}]);
+    components = dir([dirname,'/*.',varargin{1}]);
 else
     components = dir(dirname);
     components = components(3:end);
